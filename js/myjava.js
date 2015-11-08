7ae5d558979197464ab92a03add3f6943ddc3324
Fred <alfredo.aj.17@gmail.com>
2015-11-08 06:53:00 -0600

    listas

--- js/myjava.js
+++ js/myjava.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 	$('#R4').attr('disabled','disabled');
 	$('#R5').attr('disabled','disabled');
 	$('#R6').attr('disabled','disabled');
+	$('.listas').velocity({blur:30});
 	$('#b1').click(function(){
 	    $('#b1').velocity({marginLeft: -5}, {duration:1500, easing:"easelnOutExpo"});
 	    $("#b2").velocity({opacity:0, blur:30});
@@ -19,6 +20,7 @@ $(document).ready(function(){
 	    $("#b6").attr('disabled','disabled');
 	    $('#R1').velocity({marginLeft:45, opacity:1, blur:0})
 		$("#R1").removeAttr('disabled');
+		$("#L1").velocity({opacity:1, blur:0},{delay:1500});
 	  
 		$('#R1').click(function(){
 			$("#b1").velocity({marginLeft: 175}, {duration:1500, easing:"easelnOutExpo"});
@@ -34,6 +36,7 @@ $(document).ready(function(){
 		    $("#b5").removeAttr('disabled');
 		    $("#b6").velocity({opacity:1, blur:0},{delay:1500});
 		    $("#b6").removeAttr('disabled');
+		    $("#L1").velocity({opacity:0, blur:30}, 200);
 
 		  });
 		});
@@ -51,6 +54,7 @@ $(document).ready(function(){
 	    $("#b6").attr('disabled','disabled');
 	    $('#R2').velocity({marginLeft:45, opacity:1, blur:0})
 		$("#R2").removeAttr('disabled');
+		$("#L2").velocity({opacity:1, blur:0},{delay:1500});
 	
 		$('#R2').click(function(){
 			$("#b2").velocity({marginLeft: 550}, {duration:1500, easing:"easelnOutExpo"});
@@ -66,6 +70,7 @@ $(document).ready(function(){
 		    $("#b5").removeAttr('disabled');
 		    $("#b6").velocity({opacity:1, blur:0},{delay:1500});
 		    $("#b6").removeAttr('disabled');
+		    $("#L2").velocity({opacity:0, blur:30}, 200);
 	 	  });
 		});
 	$('#b3').click(function(){
@@ -82,6 +87,7 @@ $(document).ready(function(){
 	    $("#b6").attr('disabled','disabled');
 	    $('#R3').velocity({marginLeft:45, opacity:1, blur:0})
 		$("#R3").removeAttr('disabled');
+		$("#L3").velocity({opacity:1, blur:0},{delay:1500});
 	
 		$('#R3').click(function(){
 			$("#b3").velocity({marginLeft: 925}, {duration:1500, easing:"easelnOutExpo"});
@@ -97,6 +103,7 @@ $(document).ready(function(){
 		    $("#b5").removeAttr('disabled');
 		    $("#b6").velocity({opacity:1, blur:0},{delay:1500});
 		    $("#b6").removeAttr('disabled');
+		    $("#L3").velocity({opacity:0, blur:30}, 200);
 		    });
 		});
 		$('#b4').click(function(){
@@ -113,6 +120,7 @@ $(document).ready(function(){
 	    $("#b6").attr('disabled','disabled');
 	    $('#R4').velocity({marginLeft:45, opacity:1, blur:0})
 		$("#R4").removeAttr('disabled');
+		$("#L4").velocity({opacity:1, blur:0},{delay:1500});
 	  
 		$('#R4').click(function(){
 			$("#b4").velocity({marginLeft: 175, marginTop:300}, {duration:1500, easing:"easelnOutExpo"});
@@ -128,6 +136,7 @@ $(document).ready(function(){
 		    $("#b5").removeAttr('disabled');
 		    $("#b6").velocity({opacity:1, blur:0},{delay:1500});
 		    $("#b6").removeAttr('disabled');
+		    $("#L4").velocity({opacity:0, blur:30}, 200);
 
 		  });
 		});
@@ -145,6 +154,7 @@ $(document).ready(function(){
 	    $("#b6").attr('disabled','disabled');
 	    $('#R5').velocity({marginLeft:45, opacity:1, blur:0})
 		$("#R5").removeAttr('disabled');
+		$("#L5").velocity({opacity:1, blur:0},{delay:1500});
 	
 		$('#R5').click(function(){
 			$("#b5").velocity({marginLeft: 550, marginTop:300}, {duration:1500, easing:"easelnOutExpo"});
@@ -160,6 +170,7 @@ $(document).ready(function(){
 		    $("#b2").removeAttr('disabled');
 		    $("#b6").velocity({opacity:1, blur:0},{delay:1500});
 		    $("#b6").removeAttr('disabled');
+		    $("#L5").velocity({opacity:0, blur:30}, 200);
 	 	  });
 		});
 	$('#b6').click(function(){
@@ -176,6 +187,7 @@ $(document).ready(function(){
 	    $("#b3").attr('disabled','disabled');
 	    $('#R6').velocity({marginLeft:45, opacity:1, blur:0})
 		$("#R6").removeAttr('disabled');
+		$("#L6").velocity({opacity:1, blur:0},{delay:1500});
 	
 		$('#R6').click(function(){
 			$("#b6").velocity({marginLeft: 925, marginTop:300}, {duration:1500, easing:"easelnOutExpo"});
@@ -191,6 +203,7 @@ $(document).ready(function(){
 		    $("#b5").removeAttr('disabled');
 		    $("#b3").velocity({opacity:1, blur:0},{delay:1500});
 		    $("#b3").removeAttr('disabled');
+		    $("#L6").velocity({opacity:0, blur:30}, 200);
 		    });
 		});
 });
